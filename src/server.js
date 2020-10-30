fb3ddb86a6c25b6cc9533cb6076f21d8cd27b14f
Michael BAILLY <mbailly@linagora.com>
2020-10-30 09:49:58 +0100

    Switch to fastify

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { fastify } from 'fastify';
+import fastifyExpress from 'fastify-express';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+startServer();
+
+async function startServer() {
+	const server = fastify({ logger: true });
+
+	await server.register(fastifyExpress);
+	server.use(sirv('static', { dev }));
+	server.use(sapper.middleware());
+
+	await server.listen(PORT);
+}
